d0754846b26ad927dc5db38c53cbe0d3d80489df
RV1908 <134372027+RV1908@users.noreply.github.com>
2024-08-20 13:41:47 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,37 +12,6 @@ export const ABOUT_TEXT = [{text:'I am a dedicated and versatile developer with 
 {text:'I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. My coding skills have earned me high ranks in competitions such as CodeChef and LeetCode, and I have achieved notable accomplishments like qualifying for the Google Girl Hackathon 2023 and earning a 5-star gold badge in C++ on HackerRank. '},
 {text:'Outside of coding, I enjoy staying active, exploring new technologies, learning new languages and trying out new adventures.'}];
 
-// export const EXPERIENCES = [
-//   {
-//     year: "2023 - Present",
-//     role: "Senior Full Stack Developer",
-//     company: "Google Inc.",
-//     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-//     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-//   },
-//   {
-//     year: "2022 - 2023",
-//     role: "Frontend Developer",
-//     company: "Adobe",
-//     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-//     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-//   },
-//   {
-//     year: "2021 - 2022",
-//     role: "Full Stack Developer",
-//     company: "Facebook",
-//     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-//     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-//   },
-//   {
-//     year: "2020 - 2021",
-//     role: "Software Engineer",
-//     company: "Paypal",
-//     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-//     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-//   },
-// ];
-
 export const PROJECTS = [
   {
     title: "Portfolio Website",
